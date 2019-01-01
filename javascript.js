b022b8a539be0fc3f357891df8d410e88e01c905
deeno bahri <deenobahri12@gmail.com>
2019-01-01 14:37:01 -0600

    commit god

---
+++ javascript.js
@@ -0,0 +1,25 @@
+
+document.getElementById("button4").addEventListener("click", function(){
+    document.getElementById("box").style.backgroundColor = "red";
+
+});
+
+
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor = "green";
+
+    });
+
+////////////////////////////////////////////////////////////////////////////
+
+document.getElementById("btn4").addEventListener("click", function(){
+    document.getElementById("box2").style.backgroundColor = "blue";
+
+});
+
+document.getElementById("btn2").addEventListener("click", function(){
+
+    document.getElementById("box2").style.backgroundColor = "yellow";
+
+    });
